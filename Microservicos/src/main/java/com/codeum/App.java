878a73d46ac93ab55e899d97ba83664f736bc138
renanmarquetti <codeum.desenvolvimento@gmailcom>
2023-10-06 15:33:34 -0300

    reorganização da estrutura de pastas e criação dos métodos getUserFiltro e inserir, da classe UserController

--- Microservicos/src/main/java/com/codeum/App.java
+++ Microservicos/src/main/java/com/codeum/App.java
@@ -1,4 +1,4 @@
-package main;
+package com.codeum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
 	public static void main(String[] args) {
-		
+		//clean spring-boot:run
 		SpringApplication.run(App.class, args);
 		
 	}
---
+++ Microservicos/src/main/java/com/codeum/dto/UserDTO.java
@@ -0,0 +1,78 @@
+package com.codeum.dto;
+
+import java.time.LocalDateTime;
+
+
+
+public class UserDTO {
+	
+	private String nome;
+	private String cpf;
+	private String endereco;
+	private String email;
+	private String telefone;
+	private LocalDateTime dataCadastro;
+	
+	public UserDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserDTO(String nome, String cpf, String endereco, String email, String telefone, LocalDateTime dataCadastro) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+		this.endereco = endereco;
+		this.email = email;
+		this.telefone = telefone;
+		this.dataCadastro = dataCadastro;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public LocalDateTime getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(LocalDateTime dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+	
+}
